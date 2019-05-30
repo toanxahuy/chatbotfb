70700a0dbd7cf98489243759f13aac96f1148f4b
Toan XaHuy  - Senior IT Specialist - Digital Technical <toanxahuy@thp.com.vn>
2019-05-30 17:20:56 +0700

    commit 5

--- server.js
+++ server.js
@@ -60,8 +60,10 @@ function sendMessage(senderId, message) {
   });
 }
  
-app.set('port', process.env.PORT || 5000);
-app.set('ip', process.env.IP || "0.0.0.0");
+//app.set('port', process.env.PORT || 5000);
+//app.set('ip', process.env.IP || "0.0.0.0");
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
  
 server.listen(app.get('port'), app.get('ip'), function() {
   console.log("Chat bot server listening at %s:%d ", app.get('ip'), app.get('port'));
